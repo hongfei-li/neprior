92bbb46ae2fe83b14756060a25905fe11fb52849
Hongfei Li <hongfei.2.li@uconn.edu>
2018-12-03 21:20:24 -0500

    init .Rd by roxygen2

--- R/neprior.R
+++ R/neprior.R
@@ -9,14 +9,19 @@
 #' @param g A function for kernel of prior. default: g(y) = 1
 #' @param lower lower bound for y in g(y) function
 #' @param upper upper bound for y in g(y) function
+#'
 #' @return E^\pi[f(X)], \pi: g(x).
+#'
 #' @examples
 #' neprior()
+#'
 #' @examples
 #' neprior(f = function(x) {x^2}, g = function(y) {y}, lower = -10, upper = 3 )
+#'
 #' @examples
 #' norm.kernel <- function(x) {exp(- (x - 2)^2 / (2 * 3^2))} # prior: N(2,3)
 #' neprior(function(x) x^2, g = norm.kernel, lower = -Inf, upper = Inf) # E[X^2] = 9 + 2^2 = 13
+
 neprior <- function(f = function(x){x}, g = function(x){1}, lower = -Inf, upper = Inf, ...){
     ## define multiplying function.
     "%*f%" = function(x, y) {
